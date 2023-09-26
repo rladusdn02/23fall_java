367c3b38f83c2d73c8674d87e6a0b77ff9614533
kim-yeonwoo <kim-yeonwoo@218.38.137.28>
2023-09-27 00:54:32 +0900

    따옴표추가하여 오류를 없앰

--- Lab04/src/exercise/TV.java
+++ Lab04/src/exercise/TV.java
@@ -17,7 +17,7 @@ public class TV {
 	{
 		TV myTV = new TV("LG", 2023, 72);
 		myTV.show();
-		System.out.println("2211366 김연우); // 본인 학번 이름
+		System.out.println("2211366 김연우"); // 본인 학번 이름
 	}
 	
 }
