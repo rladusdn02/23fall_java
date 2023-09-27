6e1686b6eae76deac36c4474a37955040f7e2908
kim-yeonwoo <rladusdn56@gmail.com>
2023-09-27 15:19:18 +0900

    grade 완료.

--- Lab04/src/exercise/Grade.java
+++ Lab04/src/exercise/Grade.java
@@ -4,24 +4,36 @@ import java.util.Scanner;
 public class Grade 
 {
 	// 수학, 과학, 영어 점수 변수 선언
+	private int math, science, english;
 	
 	// Grade 생성자 (파라미터로 수학, 과학, 영어 가짐)
+	public Grade(int math, int science, int english) {
+		this.math=math;
+		this.science=science;
+		this.english=english;
+	}
 	
 	// 평균 구하는 함수 선언
-	
+	public double average() {
+		double a;
+		a=(double)(math+science+english)/3;
+		return a;
+	}
 	public static void main(String[] args)
 	{
 		// 실습 자료의 메인 함수와 동일
 		Scanner scan = new Scanner(System.in);
 		System.out.print("수학, 과학, 영어를 순서대로 입력하시오. (단, 정수형) : ");
 		
-		int i_math = scan.nextInt();
-		int i_science = scan.nextInt();
-		int i_english = scan.nextInt();
-		// 사용자의 입력으로부터 받아온 점수를 가지고 객체 생성
+		int math = scan.nextInt();
+		int science = scan.nextInt();
+		int english = scan.nextInt();
 		
-		Grade G_me;
+		// 사용자의 입력으로부터 받아온 점수를 가지고 객체 생성
+		Grade me= new Grade(math, science, english);
+		
 		// Grade 클래스의 메소드를 가지고 평균 출력
+		System.out.println("평균은 " + me.average());
 		
 		scan.close();
 	}
